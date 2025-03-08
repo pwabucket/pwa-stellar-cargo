c3ccbb81e0c9030b1b3404d8eddc6c118115a974
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-09 00:56:45 +0100

    Register Service Worker Immediately

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { HashRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 
 import App from "./App.jsx";
 
-const queryClient = new QueryClient();
+/** Register Service Worker */
+registerSW({ immediate: true });
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
