462e76bddf04adb29520ca218733695897513d16
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-08 08:41:04 +0100

    Aliased src directory

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     /** Plugins */
     tailwindcss(),
