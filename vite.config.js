30da24ef1e28e8a1cd216db41210c7ad94031c67
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-09 00:36:46 +0100

    Populated Manifest

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,38 @@ export default defineConfig(({ mode }) => {
       /** Plugins */
       VitePWA({
         registerType: "autoUpdate",
+        workbox: {
+          globPatterns: ["**/*.*"],
+        },
+        manifest: {
+          name: env.VITE_APP_NAME,
+          short_name: env.VITE_APP_NAME,
+          description: env.VITE_APP_DESCRIPTION,
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "pwa-64x64.png",
+              sizes: "64x64",
+              type: "image/png",
+            },
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "maskable-icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+        },
       }),
       ViteEjsPlugin(env),
       tailwindcss(),
