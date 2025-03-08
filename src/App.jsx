78045ff425b9cd4e9ab0bff323f444cda89cec35
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-08 17:21:25 +0100

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from "@/pages/Home";
+import { Route, Routes } from "react-router";
+import Account from "./pages/Account";
+import Dashboard from "./pages/Dashboard";
+import EditAccount from "./pages/EditAccount";
+import GuestRoute from "./routes/GuestRoute";
+import ImportWallet from "./pages/ImportWallet";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route element={<GuestRoute />}>
+        <Route index element={<Home />} />
+      </Route>
+      <Route element={<ProtectedRoute />}>
+        <Route path="app" element={<Dashboard />} />
+        <Route path="import" element={<ImportWallet />} />
+        <Route path="account/:publicKey/edit" element={<EditAccount />} />
+        <Route path="account/:publicKey" element={<Account />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
