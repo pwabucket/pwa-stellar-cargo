620d8a6ff09ba84027d10804db62134be2fbb88f
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-08 17:21:34 +0100

    Added QueryClient and Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+import { HashRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
