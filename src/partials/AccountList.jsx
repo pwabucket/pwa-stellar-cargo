040898e075a2887a594dfc743c0a4b0160103a88
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-08 17:45:11 +0100

    Reorder account list

--- src/partials/AccountList.jsx
+++ src/partials/AccountList.jsx
@@ -1,24 +1,54 @@
-import cn from "@/lib/utils";
 import useAppStore from "@/store/useAppStore";
+import cn, { truncatePublicKey } from "@/lib/utils";
+import { HiOutlineSquares2X2 } from "react-icons/hi2";
 import { Link } from "react-router";
+import { Reorder, useDragControls } from "motion/react";
+import { memo } from "react";
+
+const AccountReorderItem = memo(({ children, ...props }) => {
+  const dragControls = useDragControls();
+  return (
+    <Reorder.Item {...props} dragListener={false} dragControls={dragControls}>
+      <div className="flex gap-2">
+        <div className="min-w-0 min-h-0 grow">{children}</div>
+        <button
+          className={cn(
+            "bg-neutral-100 ",
+            "flex items-center justify-center",
+            "px-3 rounded-lg shrink-0",
+            "touch-none"
+          )}
+          onPointerDown={(event) => dragControls.start(event)}
+        >
+          <HiOutlineSquares2X2 className="w-4 h-4" />
+        </button>
+      </div>
+    </Reorder.Item>
+  );
+});
 
 export default function AccountList() {
   const accounts = useAppStore((state) => state.accounts);
+  const setAccounts = useAppStore((state) => state.setAccounts);
 
-  return (
-    <div className="flex flex-col gap-1">
-      {accounts.length >= 1 ? (
-        accounts.map((account) => (
+  return accounts.length >= 1 ? (
+    <Reorder.Group
+      values={accounts}
+      onReorder={(newOrder) => setAccounts(newOrder)}
+      className="flex flex-col gap-1"
+    >
+      {accounts.map((account) => (
+        <AccountReorderItem key={account.keyId} value={account}>
           <Link
             to={`/account/${account.publicKey}`}
-            key={account.keyId}
             className={cn(
               "group rounded-xl px-3 py-2",
               "bg-neutral-100",
-              "hover:bg-blue-500 hover:text-white"
+              "hover:bg-blue-500 hover:text-white",
+              "flex items-center gap-2"
             )}
           >
-            <h4 className="font-bold truncate">
+            <h4 className="font-bold truncate grow min-w-0">
               {account.name || "Stellar Account"}
             </h4>
             <p
@@ -28,13 +58,13 @@ export default function AccountList() {
                 "group-hover:text-blue-100"
               )}
             >
-              {account.publicKey}
+              {truncatePublicKey(account.publicKey)}
             </p>
           </Link>
-        ))
-      ) : (
-        <p className="text-center">No account to display</p>
-      )}
-    </div>
+        </AccountReorderItem>
+      ))}
+    </Reorder.Group>
+  ) : (
+    <p className="text-center">No account to display</p>
   );
 }
