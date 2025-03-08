eac6a7278674a4e15a5318dbbe0559e5b9a15318
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-08 08:44:28 +0100

    Configured Vite Ejs Plugin

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,31 @@
 import path from "path";
+import process from "process";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
 import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
+import { loadEnv } from "vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  /** Env */
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  plugins: [
-    /** Plugins */
-    tailwindcss(),
-    react(),
-  ],
+    plugins: [
+      /** Plugins */
+      ViteEjsPlugin(env),
+      tailwindcss(),
+      react(),
+    ],
+  };
 });
