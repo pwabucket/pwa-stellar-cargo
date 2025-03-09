d10192a09a0fc090965c9f68acfbf27d432018e7
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-09 07:36:53 +0100

    Added Menu Button Component

---
+++ src/components/MenuButton.jsx
@@ -0,0 +1,28 @@
+import cn from "@/lib/utils";
+import { memo } from "react";
+
+export default memo(function MenuButton({
+  as: Component = "button",
+  icon: Icon,
+  title = "",
+  ...props
+}) {
+  return (
+    <Component
+      {...props}
+      title={title}
+      className={cn(
+        "group rounded-xl px-3 py-2",
+        "bg-neutral-100 hover:bg-neutral-200",
+        "dark:bg-neutral-800 dark:hover:bg-neutral-700",
+        "flex items-center gap-2",
+        props.className
+      )}
+    >
+      <Icon className="size-5" />
+      <h4 className="font-bold truncate grow min-w-0 min-h-0 text-left">
+        {title}
+      </h4>
+    </Component>
+  );
+});
