5a16fd2a81bdc730381d2e038d39c6aa7ef8d073
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-09 07:38:35 +0100

    Added Link to Menu Page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,14 @@
 import AccountList from "@/partials/AccountList";
 import AppLayout from "@/layouts/AppLayout";
-import useAppStore from "@/store/useAppStore";
 import { HeaderButton } from "@/components/HeaderButton";
-import { HiOutlineArrowLeftOnRectangle, HiOutlinePlus } from "react-icons/hi2";
+import { HiOutlineBars3BottomLeft, HiOutlinePlus } from "react-icons/hi2";
 import { Link } from "react-router";
 
 export default function Dashboard() {
-  const logout = useAppStore((state) => state.logout);
   return (
     <AppLayout
       headerLeftContent={
-        <HeaderButton onClick={logout} icon={HiOutlineArrowLeftOnRectangle} />
+        <HeaderButton as={Link} to={"/menu"} icon={HiOutlineBars3BottomLeft} />
       }
       headerRightContent={
         <HeaderButton as={Link} to={"/import"} icon={HiOutlinePlus} />
