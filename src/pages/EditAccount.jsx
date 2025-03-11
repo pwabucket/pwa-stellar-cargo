f05d47bd4265651342ac9bb36d4688b038487fac
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-11 15:57:13 +0100

    Set Header Title

--- src/pages/EditAccount.jsx
+++ src/pages/EditAccount.jsx
@@ -88,6 +88,7 @@ export default function EditAccount() {
 
   return account ? (
     <InnerAppLayout
+      headerTitle="Edit Account"
       headerRightContent={
         <HeaderButton
           onClick={handleAccountRemoval}
