6ef45a2b8abcf6010ccd3f083d3bde8eb609f274
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-11 15:53:16 +0100

    Reveal Secret Key

--- src/pages/EditAccount.jsx
+++ src/pages/EditAccount.jsx
@@ -2,18 +2,22 @@ import * as yup from "yup";
 import FieldStateError from "@/components/FieldStateError";
 import FullPageSpinner from "@/components/FullPageSpinner";
 import InnerAppLayout from "@/layouts/InnerAppLayout";
+import copy from "copy-to-clipboard";
 import useAccount from "@/hooks/useAccount";
 import useAppStore from "@/store/useAppStore";
 import useCheckOrNavigate from "@/hooks/useCheckOrNavigate";
 import { Controller, FormProvider, useForm } from "react-hook-form";
 import { HeaderButton } from "@/components/HeaderButton";
-import { HiOutlineTrash } from "react-icons/hi2";
+import { HiOutlineEye, HiOutlineTrash } from "react-icons/hi2";
 import { Input } from "@/components/Input";
+import { IoCopyOutline } from "react-icons/io5";
 import { PrimaryButton } from "@/components/Button";
-import { setupKeyManager } from "@/lib/stellar/keyManager";
+import { cn } from "@/lib/utils";
+import { loadKey, setupKeyManager } from "@/lib/stellar/keyManager";
 import { useCallback } from "react";
 import { useNavigate } from "react-router";
 import { useParams } from "react-router";
+import { useState } from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 /** Schema */
@@ -23,12 +27,29 @@ const schema = yup
   })
   .required();
 
+const ToolButton = ({ icon: Icon, ...props }) => (
+  <button
+    {...props}
+    type="button"
+    className={cn(
+      "bg-neutral-100 dark:bg-neutral-800",
+      "flex items-center justify-center",
+      "px-3 rounded-xl shrink-0",
+      props.className
+    )}
+  >
+    <Icon className="size-5" />
+  </button>
+);
+
 export default function EditAccount() {
   const { publicKey } = useParams();
+  const pinCode = useAppStore((state) => state.pinCode);
   const removeAccount = useAppStore((state) => state.removeAccount);
   const updateAccount = useAppStore((state) => state.updateAccount);
   const account = useAccount(publicKey);
   const navigate = useNavigate();
+  const [secretKey, setSecretKey] = useState("");
 
   /** Form */
   const form = useForm({
@@ -47,6 +68,11 @@ export default function EditAccount() {
 
     /** Return */
     navigate(-1, { replace: true });
+  };
+
+  const revealSecretKey = async () => {
+    const result = await loadKey(account.keyId, pinCode);
+    setSecretKey(result.privateKey);
   };
 
   const handleAccountRemoval = useCallback(async () => {
@@ -93,15 +119,44 @@ export default function EditAccount() {
           />
 
           {/* Public Key */}
-          <Input
-            readOnly
-            spellCheck={false}
-            value={account.publicKey}
-            placeholder={"Public Key"}
-          />
+          <div className="flex gap-2">
+            <Input
+              readOnly
+              spellCheck={false}
+              value={account.publicKey}
+              placeholder={"Public Key"}
+              className="grow"
+            />
+
+            <ToolButton
+              icon={IoCopyOutline}
+              onClick={() => copy(account.publicKey)}
+            />
+          </div>
+
+          {/* Secret Key */}
+          <div className="flex gap-2">
+            <Input
+              readOnly
+              spellCheck={false}
+              value={secretKey}
+              placeholder={"Reveal Secret Key"}
+              className="grow"
+            />
+            {secretKey ? (
+              <ToolButton
+                icon={IoCopyOutline}
+                onClick={() => copy(secretKey)}
+              />
+            ) : (
+              <ToolButton icon={HiOutlineEye} onClick={revealSecretKey} />
+            )}
+          </div>
 
           {/* Submit Button */}
-          <PrimaryButton type="submit">Update</PrimaryButton>
+          <PrimaryButton type="submit" className="my-2">
+            Save
+          </PrimaryButton>
         </form>
       </FormProvider>
     </InnerAppLayout>
