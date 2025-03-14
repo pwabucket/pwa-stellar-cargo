d8fd5753d2b513ea5942ff1d6304d6a0225582c8
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-14 05:19:37 +0100

    [FEAT.] Select / Unselect Accounts

--- src/pages/Merge.jsx
+++ src/pages/Merge.jsx
@@ -2,9 +2,11 @@ import AccountAsset from "@/components/AccountAsset";
 import AccountBelowReserveError from "@/components/AccountBelowReserveError";
 import RequiredReserve from "@/components/RequiredReserve";
 import Spinner from "@/components/Spinner";
+import Toggle from "@/components/Toggle";
 import useAppStore from "@/store/useAppStore";
 import usePendingActivity from "@/hooks/usePendingActivity";
 import useWakeLock from "@/hooks/useWakeLock";
+import { BsFillSkipForwardCircleFill } from "react-icons/bs";
 import { HiCheckCircle, HiClock, HiXCircle } from "react-icons/hi2";
 import { PrimaryButton } from "@/components/Button";
 import { chunkArrayGenerator, cn, truncatePublicKey } from "@/lib/utils";
@@ -14,6 +16,7 @@ import {
 } from "@/lib/stellar/transactions";
 import { fetchAccountBalances, submit } from "@/lib/stellar/horizonQueries";
 import { signTransaction } from "@/lib/stellar/keyManager";
+import { useCallback } from "react";
 import { useMemo } from "react";
 import { useOutletContext } from "react-router";
 import { useState } from "react";
@@ -34,6 +37,11 @@ export default function Merge() {
     [list, publicKey]
   );
 
+  const otherAccountsMapped = useMemo(
+    () => otherAccounts.map((item) => [item.publicKey, item]),
+    [otherAccounts]
+  );
+
   const assetName =
     asset["asset_type"] === "native" ? "XLM" : asset["asset_code"];
 
@@ -43,8 +51,37 @@ export default function Merge() {
       : `${asset["asset_code"]}:${asset["asset_issuer"]}`;
 
   const [isProcessing, setIsProcessing] = useState(false);
+  const [selectedAccounts, setSelectedAccounts] = useState(
+    new Map(otherAccountsMapped)
+  );
+
   const [activeAccounts, setActiveAccounts] = useState(new Set());
   const [results, setResults] = useState(new Map());
+
+  const toggleAllAccounts = useCallback(
+    (checked) => {
+      if (checked) {
+        setSelectedAccounts(new Map(otherAccountsMapped));
+      } else {
+        setSelectedAccounts(new Map());
+      }
+    },
+    [otherAccountsMapped]
+  );
+
+  const toggleAccount = useCallback((account, checked) => {
+    if (checked) {
+      setSelectedAccounts((prev) =>
+        new Map(prev).set(account.publicKey, account)
+      );
+    } else {
+      setSelectedAccounts((prev) => {
+        const newMap = new Map(prev);
+        newMap.delete(account.publicKey);
+        return newMap;
+      });
+    }
+  }, []);
 
   /** Execute Merge */
   const executeMerge = async () => {
@@ -56,90 +93,100 @@ export default function Merge() {
     for (const chunk of chunkArrayGenerator(otherAccounts, 5)) {
       await Promise.allSettled(
         chunk.map(async (source) => {
-          try {
-            /** Mark as Active */
-            setActiveAccounts((prev) => new Set(prev).add(source.publicKey));
+          if (selectedAccounts.has(source.publicKey)) {
+            try {
+              /** Mark as Active */
+              setActiveAccounts((prev) => new Set(prev).add(source.publicKey));
 
-            /** Start Process */
-            const balances = await fetchAccountBalances(source.publicKey);
-            const sourceAssetBalance = balances.find((item) =>
-              asset["asset_type"] === "native"
-                ? asset["asset_type"] === item["asset_type"]
-                : asset["asset_issuer"] === item["asset_issuer"]
-            );
+              /** Start Process */
+              const balances = await fetchAccountBalances(source.publicKey);
+              const sourceAssetBalance = balances.find((item) =>
+                asset["asset_type"] === "native"
+                  ? asset["asset_type"] === item["asset_type"]
+                  : asset["asset_issuer"] === item["asset_issuer"]
+              );
 
-            const amount = sourceAssetBalance["balance"];
+              const amount = sourceAssetBalance["balance"];
 
-            if (amount > 0) {
-              /** Source Transaction */
-              const transaction = await createPaymentTransaction({
-                source: source.publicKey,
-                destination: account.publicKey,
-                asset: transactionAssetName,
-                amount,
-              });
+              if (amount > 0) {
+                /** Source Transaction */
+                const transaction = await createPaymentTransaction({
+                  source: source.publicKey,
+                  destination: account.publicKey,
+                  asset: transactionAssetName,
+                  amount,
+                });
 
-              const signedTransaction = await signTransaction({
-                keyId: source.keyId,
-                transactionXDR: transaction["transaction"],
-                network: transaction["network_passphrase"],
-                pinCode,
-              });
+                const signedTransaction = await signTransaction({
+                  keyId: source.keyId,
+                  transactionXDR: transaction["transaction"],
+                  network: transaction["network_passphrase"],
+                  pinCode,
+                });
 
-              /** Sponsor Transaction */
-              const feeBumpTransaction = await createFeeBumpTransaction({
-                sponsoringAccount: account.publicKey,
-                transaction: signedTransaction,
-              });
+                /** Sponsor Transaction */
+                const feeBumpTransaction = await createFeeBumpTransaction({
+                  sponsoringAccount: account.publicKey,
+                  transaction: signedTransaction,
+                });
 
-              const signedFeeBumpTransaction = await signTransaction({
-                keyId: account.keyId,
-                transactionXDR: feeBumpTransaction["transaction"],
-                network: feeBumpTransaction["network_passphrase"],
-                pinCode,
-              });
+                const signedFeeBumpTransaction = await signTransaction({
+                  keyId: account.keyId,
+                  transactionXDR: feeBumpTransaction["transaction"],
+                  network: feeBumpTransaction["network_passphrase"],
+                  pinCode,
+                });
 
-              /** Submit Sponsor Transaction */
-              const response = await submit(signedFeeBumpTransaction);
+                /** Submit Sponsor Transaction */
+                const response = await submit(signedFeeBumpTransaction);
 
-              /** Log Response */
-              console.log(response);
+                /** Log Response */
+                console.log(response);
 
-              /** Set Response */
+                /** Set Response */
+                setResults((prev) =>
+                  new Map(prev).set(source.publicKey, {
+                    status: true,
+                    balance: sourceAssetBalance,
+                    response,
+                  })
+                );
+              } else {
+                /** Set Response */
+                setResults((prev) =>
+                  new Map(prev).set(source.publicKey, {
+                    status: true,
+                    skipped: true,
+                  })
+                );
+              }
+            } catch (error) {
+              /** Log Error */
+              console.error(error);
+
+              /** Set Error */
               setResults((prev) =>
                 new Map(prev).set(source.publicKey, {
-                  status: true,
-                  balance: sourceAssetBalance,
-                  response,
+                  status: false,
+                  error,
                 })
               );
-            } else {
-              /** Set Response */
-              setResults((prev) =>
-                new Map(prev).set(source.publicKey, {
-                  status: true,
-                  skipped: true,
-                })
-              );
+            } finally {
+              /** Remove from Processing */
+              setActiveAccounts((prev) => {
+                const newSet = new Set(prev);
+                newSet.delete(source.publicKey);
+                return newSet;
+              });
             }
-          } catch (error) {
-            /** Log Error */
-            console.error(error);
-
-            /** Set Error */
+          } else {
+            /** Skip */
             setResults((prev) =>
               new Map(prev).set(source.publicKey, {
-                status: false,
-                error,
+                status: true,
+                skipped: true,
               })
             );
-          } finally {
-            /** Remove from Processing */
-            setActiveAccounts((prev) => {
-              const newSet = new Set(prev);
-              newSet.delete(source.publicKey);
-              return newSet;
-            });
           }
         })
       );
@@ -187,7 +234,7 @@ export default function Merge() {
             <>
               You are about to merge{" "}
               <span className="font-bold">{assetName}</span> from{" "}
-              <span className="font-bold">{otherAccounts.length}</span> other
+              <span className="font-bold">{selectedAccounts.size}</span> other
               account(s) into{" "}
               <span className="font-bold">
                 {account.name || "Stellar Account"} (
@@ -207,52 +254,88 @@ export default function Merge() {
       {otherAccounts.length > 0 ? (
         <>
           {/* Start Button */}
-          <PrimaryButton onClick={executeMerge} disabled={isProcessing}>
-            {isProcessing ? "Merging..." : "Proceed"}
+          <PrimaryButton
+            onClick={executeMerge}
+            disabled={isProcessing || results.size > 0}
+          >
+            {results.size > 0
+              ? "Assets Merged"
+              : isProcessing
+              ? "Merging..."
+              : "Proceed"}
           </PrimaryButton>
 
           {/* Accounts */}
+          {results.size === 0 && !isProcessing ? (
+            <label
+              className={cn(
+                "group rounded-xl px-3 py-2",
+                "bg-neutral-100 dark:bg-neutral-800",
+                "flex items-center gap-4",
+                "grow min-w-0",
+                "cursor-pointer select-none"
+              )}
+            >
+              <Toggle
+                checked={selectedAccounts.size === otherAccounts.length}
+                onChange={(ev) => toggleAllAccounts(ev.target.checked)}
+              />
+
+              {/* Account Name */}
+              <h4 className="font-bold truncate grow min-w-0">
+                Toggle Accounts
+              </h4>
+            </label>
+          ) : null}
+
           <div className="flex flex-col gap-2">
             {otherAccounts.map((source) => (
-              <div
-                key={source.keyId}
-                className={cn(
-                  "group rounded-xl px-3 py-2",
-                  "bg-neutral-100 dark:bg-neutral-800",
-                  "flex items-center gap-2"
-                )}
-              >
-                {/* Indicator */}
-                {results.has(source.publicKey) ? (
-                  results.get(source.publicKey).status ? (
-                    <HiCheckCircle
-                      className={cn(
-                        results.get(source.publicKey).skipped
-                          ? "text-orange-500"
-                          : "text-green-500",
-                        "size-5"
-                      )}
+              <div key={source.keyId} className="flex items-center gap-2">
+                <label
+                  className={cn(
+                    "group rounded-xl px-3 py-2",
+                    "bg-neutral-100 dark:bg-neutral-800",
+                    "flex items-center gap-4",
+                    "grow min-w-0",
+                    "cursor-pointer select-none"
+                  )}
+                >
+                  {/* Indicator */}
+                  {results.has(source.publicKey) ? (
+                    results.get(source.publicKey).status ? (
+                      results.get(source.publicKey).skipped ? (
+                        <BsFillSkipForwardCircleFill className="size-5 text-yellow-500" />
+                      ) : (
+                        <HiCheckCircle className="text-green-500 size-5" />
+                      )
+                    ) : (
+                      <HiXCircle className="text-red-500 size-5" />
+                    )
+                  ) : isProcessing ? (
+                    activeAccounts.has(source.publicKey) ? (
+                      <Spinner />
+                    ) : (
+                      <HiClock className="size-4" />
+                    )
+                  ) : (
+                    <Toggle
+                      checked={selectedAccounts.has(source.publicKey)}
+                      onChange={(ev) =>
+                        toggleAccount(source, ev.target.checked)
+                      }
                     />
-                  ) : (
-                    <HiXCircle className="text-red-500 size-5" />
-                  )
-                ) : isProcessing ? (
-                  activeAccounts.has(source.publicKey) ? (
-                    <Spinner />
-                  ) : (
-                    <HiClock className="size-5" />
-                  )
-                ) : null}
+                  )}
 
-                {/* Account Name */}
-                <h4 className="font-bold truncate grow min-w-0">
-                  {source.name || "Stellar Account"}
-                </h4>
+                  {/* Account Name */}
+                  <h4 className="font-bold truncate grow min-w-0">
+                    {source.name || "Stellar Account"}
+                  </h4>
 
-                {/* Account Public Key */}
-                <p className={cn("truncate", "text-xs text-blue-500")}>
-                  {truncatePublicKey(source.publicKey)}
-                </p>
+                  {/* Account Public Key */}
+                  <p className={cn("truncate", "text-xs text-blue-500")}>
+                    {truncatePublicKey(source.publicKey)}
+                  </p>
+                </label>
               </div>
             ))}
           </div>
