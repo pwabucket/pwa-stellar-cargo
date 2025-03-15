e42f316a3472caf5a8f95c1e57f399520d0f30f9
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-15 10:46:05 +0100

    Fixed Components Key

--- src/components/AssetValueMask.jsx
+++ src/components/AssetValueMask.jsx
@@ -1,6 +1,6 @@
-import repeatElement from "repeat-element";
 import useAppStore from "@/store/useAppStore";
 import { memo } from "react";
+import { repeatComponent } from "@/lib/utils";
 
 export default memo(function AssetValueMask({
   value,
@@ -12,5 +12,5 @@ export default memo(function AssetValueMask({
   const showAssetValue = useAppStore((state) => state.showAssetValue);
   return forceShow || showAssetValue
     ? `${prefix}${Intl.NumberFormat("en-US", options).format(value)}`
-    : repeatElement(<>&bull;</>, maskLength);
+    : repeatComponent(<>&bull;</>, maskLength);
 });
