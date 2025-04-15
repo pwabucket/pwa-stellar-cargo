4e448c3aef86c5a1f08175bac59234cc123918a4
Sadiq Salau <sadiqsalau888@gmail.com>
2025-04-15 17:00:08 +0100

    Place Footer Links at Bottom

--- src/partials/Welcome.jsx
+++ src/partials/Welcome.jsx
@@ -49,100 +49,102 @@ export default function Welcome() {
   };
 
   return (
-    <div className="min-h-dvh max-w-md mx-auto p-4 flex flex-col gap-4 justify-center">
-      {/* App Icon */}
-      <img src={AppIcon} className="h-24" />
+    <div className="min-h-dvh max-w-md mx-auto p-4 gap-2 flex flex-col">
+      <div className="flex flex-col grow min-w-0 min-h-0 gap-4 justify-center">
+        {/* App Icon */}
+        <img src={AppIcon} className="h-24" />
 
-      <div className="flex flex-col gap-1 px-4">
-        {/* Title */}
-        <h1 className="text-3xl text-center font-light">
-          {import.meta.env.VITE_APP_NAME}
-        </h1>
+        <div className="flex flex-col gap-1 px-4">
+          {/* Title */}
+          <h1 className="text-3xl text-center font-light">
+            {import.meta.env.VITE_APP_NAME}
+          </h1>
 
-        {/* Description */}
-        <p className="text-center text-neutral-500 dark:text-neutral-400">
-          {import.meta.env.VITE_APP_DESCRIPTION}
-        </p>
-      </div>
-
-      {accounts.length > 0 ? (
-        <div className="flex flex-col gap-2">
-          {/* Login Form */}
-          <LoginForm onVerified={onCreatedOrVerified} />
-
-          {/* Divider */}
+          {/* Description */}
           <p className="text-center text-neutral-500 dark:text-neutral-400">
-            or
+            {import.meta.env.VITE_APP_DESCRIPTION}
           </p>
-
-          {/* Reset Wallet */}
-          <button
-            onClick={resetWallet}
-            className="flex items-center justify-center gap-2"
-          >
-            <RiResetLeftFill className="size-4" />
-            Reset Wallet
-          </button>
         </div>
-      ) : showWalletForm ? (
-        <>
-          <WalletForm onCreated={onCreatedOrVerified} />
-          {/* Reset Wallet */}
-          <button
-            onClick={() => setShowWalletForm(false)}
-            className="flex items-center justify-center gap-2"
-          >
-            <HiOutlineArrowLongLeft className="size-4" />
-            Cancel Import
-          </button>
-        </>
-      ) : (
-        <>
-          <div className="flex flex-col gap-2">
-            <PrimaryButton
-              onClick={() => setShowWalletForm(true)}
-              className="text-center flex justify-center items-center gap-2"
-            >
-              <IoKeyOutline className="size-4" />
-              Import Secret Key
-            </PrimaryButton>
 
-            <SecondaryButton
-              as={Link}
-              to="/batch-import"
-              className="text-center flex justify-center items-center gap-2"
-            >
-              <IoLayersOutline className="size-4" />
-              Batch Import
-            </SecondaryButton>
+        {accounts.length > 0 ? (
+          <div className="flex flex-col gap-2">
+            {/* Login Form */}
+            <LoginForm onVerified={onCreatedOrVerified} />
 
             {/* Divider */}
             <p className="text-center text-neutral-500 dark:text-neutral-400">
               or
             </p>
 
-            {/* Restore From Google Drive */}
+            {/* Reset Wallet */}
             <button
-              disabled={googleApi.initiated === false}
-              onClick={authorizeGoogleDrive}
-              className={cn(
-                "flex items-center gap-2 justify-center disabled:opacity-50"
-              )}
+              onClick={resetWallet}
+              className="flex items-center justify-center gap-2"
             >
-              <FaGoogleDrive className="size-4" />
-              Restore from Google Drive
+              <RiResetLeftFill className="size-4" />
+              Reset Wallet
             </button>
-
-            {/* Google Backup Prompt */}
-            <GoogleBackupPrompt
-              backupFile={value}
-              open={show}
-              onOpenChange={setShow}
-              resolve={resolve}
-            />
           </div>
-        </>
-      )}
+        ) : showWalletForm ? (
+          <>
+            <WalletForm onCreated={onCreatedOrVerified} />
+            {/* Reset Wallet */}
+            <button
+              onClick={() => setShowWalletForm(false)}
+              className="flex items-center justify-center gap-2"
+            >
+              <HiOutlineArrowLongLeft className="size-4" />
+              Cancel Import
+            </button>
+          </>
+        ) : (
+          <>
+            <div className="flex flex-col gap-2">
+              <PrimaryButton
+                onClick={() => setShowWalletForm(true)}
+                className="text-center flex justify-center items-center gap-2"
+              >
+                <IoKeyOutline className="size-4" />
+                Import Secret Key
+              </PrimaryButton>
+
+              <SecondaryButton
+                as={Link}
+                to="/batch-import"
+                className="text-center flex justify-center items-center gap-2"
+              >
+                <IoLayersOutline className="size-4" />
+                Batch Import
+              </SecondaryButton>
+
+              {/* Divider */}
+              <p className="text-center text-neutral-500 dark:text-neutral-400">
+                or
+              </p>
+
+              {/* Restore From Google Drive */}
+              <button
+                disabled={googleApi.initiated === false}
+                onClick={authorizeGoogleDrive}
+                className={cn(
+                  "flex items-center gap-2 justify-center disabled:opacity-50"
+                )}
+              >
+                <FaGoogleDrive className="size-4" />
+                Restore from Google Drive
+              </button>
+
+              {/* Google Backup Prompt */}
+              <GoogleBackupPrompt
+                backupFile={value}
+                open={show}
+                onOpenChange={setShow}
+                resolve={resolve}
+              />
+            </div>
+          </>
+        )}
+      </div>
 
       <FooterLinks />
     </div>
